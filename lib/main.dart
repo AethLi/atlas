e1dd1392123c9dc3329480fce4cfabee14c9cfb0
YRJ <931622815@qq.com>
2020-04-27 17:56:50 +0800

    coding

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,5 @@
-import 'dart:async';
-
-import 'package:atlas/plugin/Smb.dart';
-import 'package:atlas/widgets/dialogs.dart';
+import 'package:atlas/widgets/page.dart';
 import 'package:flutter/material.dart';
-
-import 'widgets/mainContent.dart';
 
 void main() => runApp(MaterialApp(
       home: MainWidget(),
@@ -16,12 +11,6 @@ class MainWidget extends StatefulWidget {
 }
 
 class _MainWidgetState extends State<MainWidget> {
-  String title = "首页";
-  final State<ContentView> contentViewState = ContentGridViewState(
-    height: 60,
-    width: 80,
-  );
-
   static var globeThemeData = ThemeData(
       // Define the default brightness and colors.
       brightness: Brightness.light,
@@ -32,18 +21,6 @@ class _MainWidgetState extends State<MainWidget> {
         color: Colors.purple,
       ));
 
-  Future<void> _layoutSwitch(BuildContext context) async {
-    if (await showDialog(
-        context: context,
-        builder: (BuildContext context) {
-          double width = MediaQuery.of(context).size.width * 0.7;
-          return LayoutSwitcher(
-            width: width,
-          );
-        },
-        barrierDismissible: false)) {}
-  }
-
   final Function directoryChangeCallBack = (String title) {
     title = title;
   };
@@ -51,93 +28,8 @@ class _MainWidgetState extends State<MainWidget> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("首页"),
-          actions: <Widget>[
-            IconButton(
-              icon: Icon(Icons.format_list_bulleted),
-              onPressed: () {
-                _layoutSwitch(context);
-              },
-            )
-          ],
-        ),
-        body: Padding(
-          padding: EdgeInsets.all(10.0),
-          child: ContentView(
-              directoryChangeCallBack: directoryChangeCallBack,
-              state: contentViewState),
-        ),
-        drawer: MainDrawerWidget(
-          globeThemeData: globeThemeData,
-        ),
-      ),
+      home: ContentContainer(),
       theme: globeThemeData,
-    );
-  }
-}
-
-class MainDrawerWidget extends StatelessWidget {
-  final ThemeData globeThemeData;
-
-  static var activeNavigate = 0;
-
-  const MainDrawerWidget({Key key, @required this.globeThemeData})
-      : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Drawer(
-      child: MediaQuery.removePadding(
-        context: context,
-        removeTop: true,
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            Padding(
-              padding: const EdgeInsets.only(top: 100.0),
-              child: Row(
-                children: <Widget>[],
-              ),
-            ),
-            Expanded(
-              child: ListView(
-                children: <Widget>[
-                  ListTile(
-                    leading: const Icon(Icons.home),
-                    title: const Text('本地'),
-                    selected: activeNavigate == 0,
-                    onTap: () {
-                      activeNavigate = 0;
-                      Navigator.pop(context);
-                    },
-                  ),
-                  ListTile(
-                    leading: const Icon(Icons.forum),
-                    title: const Text('局域网Smb'),
-                    selected: activeNavigate == 1,
-                    onTap: () {
-                      Smb.getLanComputers();
-                      activeNavigate = 1;
-                      Navigator.pop(context);
-                    },
-                  ),
-                  ListTile(
-                    leading: const Icon(Icons.settings),
-                    title: const Text('设置'),
-                    selected: activeNavigate == 2,
-                    onTap: () {
-                      activeNavigate = 2;
-                      Navigator.pop(context);
-                    },
-                  )
-                ],
-              ),
-            ),
-          ],
-        ),
-      ),
     );
   }
 }
--- lib/widgets/page.dart
+++ lib/widgets/page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'dialogs.dart';
+
 class LocalStoragePage extends StatefulWidget {
   @override
   _LocalStoragePageState createState() => _LocalStoragePageState();
@@ -21,40 +23,145 @@ class IndexPage extends StatefulWidget {
   _IndexPageState createState() => _IndexPageState();
 }
 
+class _IndexMenu {
+  final String name;
+  final Function onPress;
+  final String imageAsset;
+
+  _IndexMenu(
+      {@required this.imageAsset, @required this.name, @required this.onPress});
+}
+
 class _IndexPageState extends State<IndexPage> {
+  static List<_IndexMenu> _indexMenus = [
+    _IndexMenu(
+        name: "主目录", onPress: () {}, imageAsset: "assets/images/folder.png"),
+    _IndexMenu(
+        name: "根目录", onPress: () {}, imageAsset: "assets/images/folder.png"),
+    _IndexMenu(
+        name: "局域网共享",
+        onPress: () {},
+        imageAsset: "assets/images/lan.png"),
+    _IndexMenu(
+        name: "FTP", onPress: () {}, imageAsset: "assets/images/ftp.png"),
+  ];
+
   @override
   Widget build(BuildContext context) {
-    return ListView(
+    return Column(
       children: [
         Container(
-          height: 200,
+          height: MediaQuery.of(context).size.height * 0.3,
         ),
-        GridView(
-          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-              crossAxisCount: 3, childAspectRatio: 1.1),
-          children: [
-            Column(
-              children: <Widget>[
-                Container(
-                    margin: EdgeInsets.fromLTRB(0, 5, 0, 0),
-                    child: Center(
-                      child: Image.asset(
-                        "assets/images/directory.png",
-                        fit: BoxFit.contain,
+        Divider(
+          height: 5,
+          color: Colors.purple,
+          thickness: 1,
+        ),
+        Expanded(
+          child: GridView.builder(
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 4, childAspectRatio: 1),
+              itemCount: _indexMenus.length,
+              itemBuilder: (context, index) {
+                return FlatButton(
+                  child: Column(
+                    children: <Widget>[
+                      Expanded(
+                        child: Container(
+                            margin: EdgeInsets.fromLTRB(0, 5, 0, 0),
+                            child: Center(
+                              child: ColorFiltered(
+                                child: Image.asset(
+                                  _indexMenus[index].imageAsset,
+                                  fit: BoxFit.contain,
+                                ),
+                                colorFilter: ColorFilter.mode(Colors.purple, BlendMode.srcATop),
+                              ),
+                            )),
                       ),
-                    )),
-                Container(
-                  margin: EdgeInsets.fromLTRB(0, 5, 0, 0),
-                  child: Text(
-                    "name",
-                    textAlign: TextAlign.center,
+                      Container(
+                        margin: EdgeInsets.fromLTRB(0, 5, 0, 0),
+                        child: Text(
+                          _indexMenus[index].name,
+                          textAlign: TextAlign.center,
+                          maxLines: 1,
+                          overflow: TextOverflow.ellipsis,
+                        ),
+                      )
+                    ],
                   ),
-                )
-              ],
-            ),
-          ],
+                  onPressed: () {},
+                );
+              }),
         )
       ],
+    );
+  }
+}
+
+class ContentContainer extends StatefulWidget {
+  @override
+  _ContentContainerState createState() => _ContentContainerState();
+}
+
+class _ContentContainerState extends State<ContentContainer> {
+  Future<void> _layoutSwitch(BuildContext context) async {
+    if (await showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          double width = MediaQuery.of(context).size.width * 0.7;
+          return LayoutSwitcher(
+            width: width,
+          );
+        },
+        barrierDismissible: false)) {}
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Container(
+          width: MediaQuery.of(context).size.width * 0.4,
+          child: DropdownButton<String>(
+            value: "One",
+            dropdownColor: Colors.purple,
+            icon: Icon(
+              Icons.keyboard_arrow_down,
+              color: Colors.white,
+            ),
+            style: TextStyle(color: Colors.white),
+            onChanged: (String newValue) {
+              setState(() {});
+            },
+            items: <String>['One', 'Two', 'Free', 'Four']
+                .map<DropdownMenuItem<String>>((String value) {
+              return DropdownMenuItem<String>(
+                value: value,
+                child: Text(value),
+              );
+            }).toList(),
+          ),
+        ),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.home),
+            onPressed: () {},
+          ),
+          IconButton(
+            icon: Icon(Icons.search),
+            onPressed: () {},
+          ),
+          IconButton(
+            icon: Icon(Icons.grid_on),
+            onPressed: () {
+              _layoutSwitch(context);
+            },
+          )
+        ],
+      ),
+      body: IndexPage(),
     );
   }
 }
